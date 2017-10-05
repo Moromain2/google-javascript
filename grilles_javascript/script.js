6296c0e8cb5f0b448058296e30186cc20fcebc0b
Romain Mosser <mosser.romain@gmail.com>
2017-10-05 17:49:59 +0200

    ajout des grilles

---
+++ grilles_javascript/script.js
@@ -0,0 +1,17 @@
+var N = prompt("Choisissez un chiffre entre 1 et 70")
+/*console.log(N);
+
+// objectif : faire apparaitre les td et tr en fonction de N
+
+let grid = document.getElementById('tableau');
+
+let tabRow = document.createElement('<tr></tr>');
+let tabD = document.createElement('<td></td>');
+
+let i=0;
+
+function generateGrid() {
+  for (i=0; i<=N; i++) {
+      document.write("<td></td>" * i)
+  }
+}*/
